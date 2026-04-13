a6e5cf30ad8d536db9362664312e27339cbaa1ee
Sameer <sk6586418@gmail.com>
2026-04-13 23:17:14 +0530

    feat: implement user registration and login functionality with JWT authentication

---
+++ Backend/src/controllers/auth.controller.js
@@ -0,0 +1,115 @@
+const userModel = require("../models/user.model");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+async function registerUser(req, res) {
+    const { username, email, password } = req.body;
+
+    const isAlreadyRegister = await userModel.findOne({
+        $or: [
+            { username },
+            { email }
+        ]
+    });
+
+    if (isAlreadyRegister) {
+        let message = "User already exists";
+
+        const emailMatch = isAlreadyRegister.email === email;
+        const usernameMatch = isAlreadyRegister.username === username;
+
+        if (emailMatch && usernameMatch) {
+            message += "with this email and username";
+        } else if (emailMatch) {
+            message += "with this email address";
+        } else if (usernameMatch) {
+            message += "with this username";
+        }
+
+        return res.status(400).json({ message })
+    }
+
+    const hash = await bcrypt.hash(password, 10);
+
+    const user = await userModel.create({
+        username,
+        email,
+        password: hash
+    });
+
+    const token = jwt.sign(
+        {
+            id: user._id,
+            username: user.username
+        },
+        process.env.JWT_SECRET,
+        {
+            expiresIn: "1d"
+        }
+    );
+
+    res.cookie("token", token);
+
+    return res.status(201).json({
+        message: "User Register Successfully",
+        user: {
+            id: user._id,
+            username: user.username,
+            email: user.email
+        }
+    })
+
+}
+
+async function loginUser(req, res) {
+    const { email, username, password } = req.body;
+
+    const user = await userModel.findOne({
+        $or: [
+            { email },
+            { username }
+        ]
+    });
+
+    if (!user) {
+        res.status(400).json({
+            message: "Invalid credentials"
+        })
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+        res.status(400).json({
+            message: "Invalid credentials"
+        })
+    }
+
+    const token = jwt.sign(
+        {
+            id: user._id,
+            username: user.username
+        },
+        process.env.JWT_SECRET,
+        {
+            expiresIn: "1d"
+        }
+    );
+
+    res.cookie("token", token);
+
+    return res.status(200).json({
+        message: "User logged in successfully",
+        user:{
+            id: user._id,
+            username: user.username,
+            email: user.username
+        }
+    })
+
+}
+
+module.exports = {
+    registerUser,
+    loginUser
+}
---
+++ Backend/src/models/user.model.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: [true, "Username is required"],
+        unique: [true, "Username must be Unique"]
+    },
+    email: {
+        type: String,
+        required: [true, "Email is required"],
+        unique: [true, "Email must be unique"]
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required"]
+    }
+}, {
+    timestamps: true
+});
+
+// TASK 
+// userSchema.pre("save", function (next) { })
+// userSchema.post("save", function (next) { })
+
+const userModel = mongoose.model("users", userSchema);
+
+module.exports = userModel;
