a0329a8c3734210075f4cbe394b88bc13b5146ce
Sameer <sk6586418@gmail.com>
2026-04-13 16:16:00 +0530

    fix: handle database connection errors in connectToDb function

--- Backend/src/config/database.js
+++ Backend/src/config/database.js
@@ -1,9 +1,13 @@
 const mongooes = require("mongoose");
 
 function connectToDb() {
-    mongooes.connect(process.env.MONGO_URI).then(()=>{
-        console.log("Connect to DB");
-    });
+    mongooes.connect(process.env.MONGO_URI)
+        .then(() => {
+            console.log("Connect to DB");
+        })
+        .catch(err => {
+            console.log("Error connecting to DB", err);
+        })
 }
 
 module.exports = connectToDb;
