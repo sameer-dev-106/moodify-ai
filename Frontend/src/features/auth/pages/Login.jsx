91a093cf5afe00551717d2f42966451b9e8cd83d
Sameer <sk6586418@gmail.com>
2026-04-20 21:14:50 +0530

    feat: enhance Login and Register components with form submission handling and loading state

--- Frontend/src/features/auth/pages/Login.jsx
+++ Frontend/src/features/auth/pages/Login.jsx
@@ -1,22 +1,53 @@
-import React from "react";
-import "../style/login.scss";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router";
 import FormGroup from "../components/FormGroup";
-import { Link } from "react-router";
+import { useAuth } from "../hooks/useAuth";
+
+import "../style/login.scss";
 
 const Login = () => {
+  const { loading, handleLogin } = useAuth();
+
+  const navigate = useNavigate();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    await handleLogin({ email, password });
+    navigate("/");
+  }
+
+  if (loading) {
+    return (
+      <main>
+        <h1>Loading....</h1>
+      </main>
+    );
+  }
+
   return (
     <main className="login-page">
       <div className="form-container">
         <h1>Login</h1>
-        <form>
+        <form onSubmit={handleSubmit}>
           <FormGroup
             type="email"
             label="Email"
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
             placeholder="Enter your email"
           />
           <FormGroup
             type="password"
             label="Password"
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
             placeholder="Enter your password"
           />
           <button type="submit">Login</button>
--- Frontend/src/features/auth/pages/Register.jsx
+++ Frontend/src/features/auth/pages/Register.jsx
@@ -1,27 +1,63 @@
-import React from "react";
-import "../style/register.scss";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router";
 import FormGroup from "../components/FormGroup";
-import { Link } from "react-router";
+import { useAuth } from "../hooks/useAuth";
+
+import "../style/register.scss";
 
 const Register = () => {
+  const { loading, handleRegister } = useAuth();
+
+  const navigate = useNavigate();
+
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    await handleRegister({ username, email, password });
+    navigate("/");
+  }
+
+  if (loading) {
+    return (
+      <main>
+        <h1>Loading...</h1>
+      </main>
+    );
+  }
+
   return (
     <main className="register-page">
       <div className="form-container">
         <h1>Register</h1>
-        <form>
+        <form onSubmit={handleSubmit}>
           <FormGroup
             type="text"
             label="Username"
+            value={username}
+            onChange={(e) => {
+              setUsername(e.target.value);
+            }}
             placeholder="Enter your username"
           />
           <FormGroup
             type="email"
             label="Email"
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
             placeholder="Enter your email"
           />
           <FormGroup
             type="password"
             label="Password"
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
             placeholder="Enter your password"
           />
           <button type="submit">Register</button>
