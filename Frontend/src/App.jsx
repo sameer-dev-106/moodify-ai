aff91e4056313cf2170f6c9529d23c46c836d4c5
Sameer <sk6586418@gmail.com>
2026-04-22 23:12:11 +0530

    feat: wrap RouterProvider with SongContextProvider for improved context management

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,16 @@
 import { RouterProvider } from "react-router";
 import { router } from "./app.routes";
 import { AuthProvider } from "./features/auth/auth.context";
+import { SongContextProvider } from "./features/home/Song.context";
 
 import "./styles/global.scss";
 
 const App = () => {
   return (
     <AuthProvider>
-      <RouterProvider router={router} />
+      <SongContextProvider>
+        <RouterProvider router={router} />
+      </SongContextProvider>
     </AuthProvider>
   );
 };
